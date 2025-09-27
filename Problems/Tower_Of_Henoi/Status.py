21cca2b72e6250d92d4da02494163b6d13d9e165
Bhardwaj <bhardwajsheojeerai@gmail.com>
2025-09-27 19:04:51 +0530

    add replay

--- Problems/Tower_Of_Henoi/Status.py
+++ Problems/Tower_Of_Henoi/Status.py
@@ -10,18 +10,25 @@ class Status:
         self.total_step = (2 ** self.num_disc) - 1
         self.remaining = self.total_step
         self.steps = Turtle()
-        self.steps.color("green")
+        self.steps.color("cyan")
         self.steps.hideturtle()
         self.steps.penup()
         self.steps.goto(-430, 260)
         self.update_status()
 
         self.hint = Turtle()
-        self.hint.color("green")
+        self.hint.color("cyan")
         self.hint.hideturtle()
         self.hint.penup()
         self.hint.goto(430, 260)
         self.hint.write("Hint: H", align= Align, font= Font2)
+
+        self.replay = Turtle()
+        self.replay.color("cyan")
+        self.replay.hideturtle()
+        self.replay.penup()
+        self.replay.goto(430, 230)
+        self.replay.write("Replay: R", align=Align, font=Font2)
 
     def update_status(self):
         self.steps.clear()
