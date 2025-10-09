438a89cd0778c1206664256b78cc3f379552573d
Bhardwaj <bhardwajsheojeerai@gmail.com>
2025-10-09 20:31:03 +0530

    delete other files

--- PYTHON/Basics/Flow_Of_Program/Multiplication.py
+++
@@ -1,7 +0,0 @@
-# Take a number as input and print the multiplication table for it.
-def multiplication_table(number):
-    for i in range(1, 11):
-        print(f"{number} X {i} = {number * i}")
-
-num = int(input("Number: "))
-multiplication_table(num)
